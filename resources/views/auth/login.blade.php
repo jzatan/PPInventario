eeb40fc3ced7270af9dc4ae50ecf26ee4f9ca636
Knythe <krondoyv@ucvvirtual.edu.pe>
2024-10-10 00:14:14 -0500

    registro-delete-usuarios and login_desing

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,5 @@
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
   <meta charset="utf-8" />
@@ -8,7 +7,7 @@
   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="../assets/img/favicon.png">
   <title>
-    Argon Dashboard 2 by Creative Tim
+    Login
   </title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
@@ -28,53 +27,7 @@
     <div class="row">
       <div class="col-12">
         <!-- Navbar -->
-        <nav class="navbar navbar-expand-lg blur border-radius-lg top-0 z-index-3 shadow position-absolute mt-4 py-2 start-0 end-0 mx-4">
-          <div class="container-fluid">
-            <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="../pages/dashboard.html">
-              Argon Dashboard 2
-            </a>
-            <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon mt-2">
-                <span class="navbar-toggler-bar bar1"></span>
-                <span class="navbar-toggler-bar bar2"></span>
-                <span class="navbar-toggler-bar bar3"></span>
-              </span>
-            </button>
-            <div class="collapse navbar-collapse" id="navigation">
-              <ul class="navbar-nav mx-auto">
-                <li class="nav-item">
-                  <a class="nav-link d-flex align-items-center me-2 active" aria-current="page" href="../pages/dashboard.html">
-                    <i class="fa fa-chart-pie opacity-6 text-dark me-1"></i>
-                    Dashboard
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link me-2" href="../pages/profile.html">
-                    <i class="fa fa-user opacity-6 text-dark me-1"></i>
-                    Profile
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link me-2" href="../pages/sign-up.html">
-                    <i class="fas fa-user-circle opacity-6 text-dark me-1"></i>
-                    Sign Up
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link me-2" href="../pages/sign-in.html">
-                    <i class="fas fa-key opacity-6 text-dark me-1"></i>
-                    Sign In
-                  </a>
-                </li>
-              </ul>
-              <ul class="navbar-nav d-lg-block d-none">
-                <li class="nav-item">
-                  <a href="https://www.creative-tim.com/product/argon-dashboard" class="btn btn-sm mb-0 me-1 btn-primary">Free Download</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+       
         <!-- End Navbar -->
       </div>
     </div>
@@ -82,45 +35,69 @@
   <main class="main-content  mt-0">
     <section>
       <div class="page-header min-vh-100">
-        <div class="container">
+        <div class="container-fluid">
           <div class="row">
-            <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
+            <div class="col-xl- col-lg- col-md-4 d-flex flex-column mx-lg-0 mx-auto">
               <div class="card card-plain">
-                <div class="card-header pb-0 text-start">
-                  <h4 class="font-weight-bolder">Sign In</h4>
-                  <p class="mb-0">Enter your email and password to sign in</p>
-                </div>
-                <div class="card-body">
-                  <form role="form">
-                    <div class="mb-3">
-                      <input type="email" class="form-control form-control-lg" placeholder="Email" aria-label="Email">
-                    </div>
-                    <div class="mb-3">
-                      <input type="email" class="form-control form-control-lg" placeholder="Password" aria-label="Password">
-                    </div>
-                    <div class="form-check form-switch">
-                      <input class="form-check-input" type="checkbox" id="rememberMe">
-                      <label class="form-check-label" for="rememberMe">Remember me</label>
-                    </div>
-                    <div class="text-center">
-                      <button type="button" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Sign in</button>
-                    </div>
-                  </form>
-                </div>
-                <div class="card-footer text-center pt-0 px-lg-2 px-1">
-                  <p class="mb-4 text-sm mx-auto">
-                    Don't have an account?
-                    <a href="javascript:;" class="text-primary text-gradient font-weight-bold">Sign up</a>
+                <div class="card-header pb-0 text-center">
+                <img src="assets\img\log-login.png" alt="Logo" class="img-fluid" style="width: 150px; height:auto; margin-bottom: 30px;"> 
+                 
+                  <h4 class="font-weight-bolder">
+                    Inicia sesión
+                  </h4>
+                  <p class="mb-0">
+                    Ingrese su correo electrónico y contraseña para iniciar sesión
                   </p>
                 </div>
+                <div class="card-body">
+
+                
+                <form role="form">
+                  <div class="mb-3">
+                      <input type="email" class="form-control form-control-lg" placeholder="Correo" aria-label="email">
+                  </div>
+                  <div class="mb-3 position-relative">
+                  <input type="password" id="password" class="form-control form-control-lg" placeholder="Contraseña" aria-label="contraseña"    autocomplete="off">
+                  <button type="button" id="togglePassword" class="btn btn-sm btn-outline-secondary position-absolute" style="top: 50%; right: 10px; transform: translateY(-50%);">
+                          Mostrar
+                      </button>
+                  </div>
+
+                  <div class="form-check form-switch">
+                      <input class="form-check-input" type="checkbox" id="rememberMe">
+                      <label class="form-check-label" for="rememberMe">
+                          Recuérdame
+                      </label>
+                  </div>
+                  <div class="text-center">
+                      <button type="button" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">
+                          Inicia sesión
+                      </button>
+                  </div>
+              </form>
+
+              <script>
+                  const togglePasswordButton = document.getElementById('togglePassword');
+                  const passwordInput = document.getElementById('password');
+
+                  togglePasswordButton.addEventListener('click', function () {
+                      // Cambia el tipo del input
+                      if (passwordInput.type === 'password') {
+                          passwordInput.type = 'text';
+                          togglePasswordButton.textContent = 'Ocultar';
+                      } else {
+                          passwordInput.type = 'password';
+                          togglePasswordButton.textContent = 'Mostrar';
+                      }
+                  });
+              </script>
+
               </div>
             </div>
-            <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
-              <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="background-image: url('/assets/img/fondo-prueba.jpg');
-          background-size: cover;">
-                <span class="mask bg-gradient-primary opacity-6"></span>
-                <h4 class="mt-5 text-white font-weight-bolder position-relative">"Attention is the new currency"</h4>
-                <p class="text-white position-relative">The more effortless the writing looks, the more effort the writer actually put into the process.</p>
+            <div class="col-6  col-md-8 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
+              <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="background-image: url('/assets/img/portada.png'); background-size: cover;">
+                <span class="mask bg-gradient-primary opacity-3"></span>
+               
               </div>
             </div>
           </div>
